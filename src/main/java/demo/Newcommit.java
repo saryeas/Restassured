2c39974a78c9cad6dffab949164fc578813ea540
sarmin <class@gmail.com>
2023-11-02 17:02:14 -0500

    new push

---
+++ src/main/java/demo/Newcommit.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class Newcommit {
+
+	public void New() { 
+		System.out.println("Newcommit");
+		}
+	}
+
--- src/main/java/tests/TestExample.java
+++ src/main/java/tests/TestExample.java
@@ -2,12 +2,11 @@ package tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-
 public class TestExample {
-	@Test
+	
+
 	public void test_1() {
 		Response res = RestAssured.get("https://reqres.in/api/users?page=2");
 		
@@ -23,14 +22,5 @@ public class TestExample {
 		
 		Assert.assertEquals(statusCode, 201);
 	}
-	//@Test
-		void test_02() {
-			
-			given()
-				.get("https://reqres.in/api/users?page=2")
-			.then()
-				.statusCode(200)
-				.body("data.id[0]", equalTo(9));
-		}
-
+	
 }
